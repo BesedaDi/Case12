67eec0e1a10abad99d71b806a11ca6c63e14cf87
BesedaDi <dianabesedina223@gmail.com>
2020-02-26 20:39:51 +0700

    start

--- filesystem.py
+++ filesystem.py
@@ -1,6 +1,14 @@
 # Case-study #12
 # Developers:   Besedina D. (%),
 #               Setskov M.  (%).
+# print("1. Просмотр каталога") #Direc_brow
+# print("2. На уровень вверх") #level_up
+# print("3. На уровень вниз") #Level_down
+# print("4. Количество файлов и каталогов") #Quantity
+# print("5. Размер текущего каталога (в байтах)") #size
+# print("6. Поиск файла") #Search
+# print("7. Выход из программы") #Exit
+# command=input("Выберите пункт меню:")
 def Direc_brow():
     pass
 
