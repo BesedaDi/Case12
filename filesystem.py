a7bfceeecc89b927197e17b599e1c93e99855464
BesedaDi <dianabesedina223@gmail.com>
2020-02-26 14:36:07 +0700

    start

---
+++ filesystem.py
@@ -0,0 +1,42 @@
+# Case-study #12
+# Developers:   Besedina D. (%),
+#               
+print("1. Просмотр каталога") #Direc_brow
+print("2. На уровень вверх") #level_up
+print("3. На уровень вниз") #Level_down
+print("4. Количество файлов и каталогов") #Quantity
+print("5. Размер текущего каталога (в байтах)") #size
+print("6. Поиск файла") #Search_цикл
+print("7. Выход из программы") #Exit
+command=input("Выберите пункт меню:")
+if command!=1 or command!=2 or command!=3 or command!=4 or command!=5 or command!=6 or command!=7:
+    print("ERROR")
+import os
+if command==1:
+    def Direc_brow():
+        pass
+    pass
+elif command==2:
+    def level_up():
+        pass
+    pass
+elif command==3:
+    def Level_down():
+        pass
+    pass
+elif command==4:
+    def Quantity():
+        pass
+    pass
+elif command==5:
+    def size():
+        pass
+    pass
+elif command==6:
+    def Search():
+        pass
+    pass
+elif command==7:
+    def Exit():
+        pass
+    pass
--- test1.py
+++ test1.py
@@ -1,3 +1,6 @@
+# Case-study #12
+# Developers:   Besedina D. (%),
+#
 # Программа которая позволяет пользоватею получать информацию о файлоовй сиситеме
 # Терминальная навигация в файловой системе
 print("1. Просмотр каталога") #Direc_brow
@@ -5,11 +8,12 @@ print("2. На уровень вверх") #level_up
 print("3. На уровень вниз") #Level_down
 print("4. Количество файлов и каталогов") #Quantity
 print("5. Размер текущего каталога (в байтах)") #size
-print("6. Поиск файла") #Search
+print("6. Поиск файла") #Search_цикл
 print("7. Выход из программы") #Exit
 command=input("Выберите пункт меню:")
 if command!=1 or command!=2 or command!=3 or command!=4 or command!=5 or command!=6 or command!=7:
     print("ERROR")
+import os
 if command==1:
     def Direc_brow():
         pass
