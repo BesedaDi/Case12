afb76bad8463958d55bc08a466256c31665123c2
BesedaDi <dianabesedina223@gmail.com>
2020-02-26 21:39:01 +0700

    start

--- filesystem.py
+++ filesystem.py
@@ -1,53 +1,27 @@
 # Case-study #12
 # Developers:   Besedina D. (%),
 #               Setskov M.  (%).
-# print("1. Просмотр каталога") #Direc_brow
-# print("2. На уровень вверх") #level_up
-# print("3. На уровень вниз") #Level_down
-# print("4. Количество файлов и каталогов") #Quantity
-# print("5. Размер текущего каталога (в байтах)") #size
-# print("6. Поиск файла") #Search
-# print("7. Выход из программы") #Exit
-# command=input("Выберите пункт меню:")
-def Direc_brow():
+def acceptCommand():
     pass
-
-pass
-
-def level_up():
+def runCommand(command):
     pass
-
-pass
-
-def Level_down():
+def moveUp():
     pass
-
-pass
-
-
-def Quantity():
+def moveDown():
     pass
-
-pass
-
-
-def size():
+def countFiles(path):
     pass
-
-pass
-
-
-def Search():
-    name = input()
-    tree = os.walk(name)
-pass
-
-
-def Exit():
+def countBytes(path):
     pass
-
-pass
+def findFiles(target,path):
+    pass
 
 def main():
-
-main()
+    while True:
+        print(os.getcwd())
+        print(MENU)
+        command=acceptCommand()
+        runCommand(command)
+        if command == QUIT:
+            print('Работа программы завершена.')
+            break
