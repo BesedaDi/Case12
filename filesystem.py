6c95e86e7ebaaf2a5173473125d24f5b97f4cec5
Sampli <maksim3.ty@gmail.com>
2020-03-09 13:51:57 +0700

    movedown+

--- filesystem.py
+++ filesystem.py
@@ -1,20 +1,40 @@
 # Case-study #12
 # Developers:   Besedina D. (%),
 #               Setskov M.  (%).
+import os
+import os.path
+
 def acceptCommand():
     pass
+
 def runCommand(command):
     pass
+
 def moveUp():
     pass
+
 def moveDown():
-    pass
+    newDir = input('Введите название нужного вам подкаталога: ')
+    dir_now = os.getcwd()
+    while True:
+        try:
+            os.chdir(dir_now + '\\' + newDir)
+            break
+        except:
+            print('Такого подкаталога не существует.')
+            newDir = input('Введите корректное название католога: ')
+    print('Вы находитесь в данном каталоге:')
+    print(os.getcwd())
+
 def countFiles(path):
     pass
+
 def countBytes(path):
     pass
+
 def findFiles(target,path):
     pass
+
 
 def main():
     while True:
